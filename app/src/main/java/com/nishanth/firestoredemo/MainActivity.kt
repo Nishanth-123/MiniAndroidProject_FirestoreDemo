48d0876926694f13a3212c6ade8edb17a880bb54
Nishanth12321 <nishanth@risor.com>
2021-10-08 18:12:22 +0530

    Adding multiple documents

--- app/src/main/java/com/nishanth/firestoredemo/MainActivity.kt
+++ app/src/main/java/com/nishanth/firestoredemo/MainActivity.kt
@@ -9,8 +9,11 @@ import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -34,6 +37,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        collectionRef.addSnapshotListener(this, object:EventListener<QuerySnapshot>{
+            override fun onEvent(querySnapshot: QuerySnapshot?, e: FirebaseFirestoreException?) {
+                if (e != null) {
+                    result.setText(e.message.toString())
+                    return
+                }
+                val notesData=""
+                querySnapshots?.let{
+                    for(querySnapshot in it ){
+                        val note:Note=querySnapshot.toObject(Note.class)
+                                notesData+="Title : ${note.title}\\n\" + \"Title : ${note.description}\n"
+                    }
+                }
+                result.setText(notesData)
+            }
+
+        })
+        /*
         noteRef.addSnapshotListener(this) { documentSnapshot, e ->
             //exception may arise when accessing the doc is not possible
             if (e != null) {
@@ -48,16 +69,20 @@ class MainActivity : AppCompatActivity() {
                 result.setText("")
             }
         }
+
+         */
     }
 
-    fun saveData(view: View) {
+    fun addNote(view: View) {
         val note = Note(title.text.toString(), description.text.toString())
+        collectionRef.add(note)
         /*
         val note = HashMap<String, Any>()
         note[TITLE] = title.text.toString()
         note[DESCRIPTION] = description.text.toString()
 
          */
+        /*
         noteRef.set(note)
                 .addOnSuccessListener {
                     Toast.makeText(this, "saved successfully", LENGTH_SHORT).show()
@@ -65,6 +90,8 @@ class MainActivity : AppCompatActivity() {
                 .addOnFailureListener {
                     Toast.makeText(this, it.message.toString(), LENGTH_SHORT).show()
                 }
+
+         */
         /*db.collection("Notebook").document("First Note").set(note)
                 .addOnSuccessListener {
                     Toast.makeText(this, "saved successfully", LENGTH_SHORT).show()
@@ -74,13 +101,14 @@ class MainActivity : AppCompatActivity() {
                 }*/
     }
 
-    fun fetchData(view: View) {
+    fun loadNotes(view: View) {
+        /*
         noteRef.get()
                 .addOnSuccessListener {
                     if (it.exists()) {
                         //it.data returns the map we had passed
                         //result.setText("Title : ${it.getString(TITLE)}\n" + "Title : ${it.getString(DESCRIPTION)}\n")
-                        val note:Note = it.toObject(Note::class.java)
+                        val note:Note = it.toObject(Note.class)
                         result . setText ("Title : ${note.title}\n" + "Title : ${note.getString(DESCRIPTION)}\n")
                     } else {
                         result.setText("Document does not exist")
@@ -89,8 +117,24 @@ class MainActivity : AppCompatActivity() {
                 .addOnFailureListener {
                     result.setText(it.message.toString())
                 }
-    }
 
+         */
+        collectionRef.get()
+                .addOnSuccessListener(object:OnSuccessListener<QuerySnapshot>{
+                    override fun onSuccess(querySnapshots: QuerySnapshot?) {
+                        val notesData=""
+                        querySnapshots?.let{
+                            for(querySnapshot in it ){
+                                val note:Note=querySnapshot.toObject(Note.class)
+                                notesData+="Title : ${note.title}\\n\" + \"Title : ${note.description}\n"
+                            }
+                        }
+                        result.setText(notesData)
+                    }
+
+                })
+    }
+/*
     fun deleteDescription(view: View) {
         /*val note = HashMap<String, Any>()
         note[DESCRIPTION] = description.text.toString()
@@ -101,4 +145,6 @@ class MainActivity : AppCompatActivity() {
     fun deleteNotes(view: View) {
         noteRef.delete()
     }
+
+ */
 }
